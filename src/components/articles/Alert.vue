61981e34d54f97fbb740f06819cb1a6935b6bdba
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-05-21 18:58:28 -0500

    feat new Alert component

---
+++ src/components/articles/Alert.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="alert alert-danger alert__error" role="alert">
+      {{text}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: ['text']
+}
+</script>
+
+<style scoped>
+.alert__error {
+  position: fixed;
+  right: 5px;
+  top: 5px;
+}
+</style>
