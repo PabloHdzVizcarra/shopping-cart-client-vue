90748a764d0727be83fce06ef59538ee0073ae8b
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-05-21 17:09:14 -0500

    feat create basic FormArticle

--- src/components/articles/FormArticle.vue
+++ src/components/articles/FormArticle.vue
@@ -1,5 +1,16 @@
 <template>
-<p>form article</p>
+  <form class="p-4 text-start">
+    <label for="article-name" class="form-label">Name</label>
+    <input type="text" id="article-name" class="form-control mb-2">
+
+    <label for="article-price" class="form-label">Price</label>
+    <input type="number" id="article-price" class="form-control mb-2">
+
+    <label for="article-brand" class="form-label">Brand</label>
+    <input type="text" id="article-brand" class="form-control mb-2">
+
+    <button type="submit" class="btn btn-success">Save</button>
+  </form>
 </template>
 
 <script>
