1c2bab79990d7168830880b52ddb220bdb59283b
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-05-22 12:38:57 -0500

    test added more test

--- src/tests/unit/FormArticle.spec.js
+++ src/tests/unit/FormArticle.spec.js
@@ -19,24 +19,24 @@ describe('tests in FormArticle component', () => {
     expect(EventValidateError).toHaveLength(1)
   })
 
-  // test('fields must be cleared after the correct data has been added to the form', () => {
-  //   const wrapper = mount(FormArticle)
-  //   const button = wrapper.find('button')
-  //   button.trigger('submit')
-  //
-  //   const inputName = wrapper.findAll('input')[0]
-  //   const inputPrice = wrapper.findAll('input')[1]
-  //   const inputBrand = wrapper.findAll('input')[2]
-  //
-  //   inputName.setValue('milk')
-  //   inputPrice.setValue(15)
-  //   inputBrand.setValue('Lala')
-  //
-  //   button.trigger('submit')
-  //
-  //   expect(wrapper.emitted()).toHaveProperty('save-article')
-  //   expect(inputName.element.value).toBe('')
-  //   expect(inputPrice.element.value).toEqual(0)
-  //   expect(inputBrand.element.value).toEqual('')
-  // })
+  test('fields must be cleared after the correct data has been added to the form', () => {
+    const wrapper = mount(FormArticle)
+    const button = wrapper.find('button')
+    button.trigger('submit')
+
+    const inputName = wrapper.findAll('input')[0]
+    const inputPrice = wrapper.findAll('input')[1]
+    const inputBrand = wrapper.findAll('input')[2]
+
+    inputName.setValue('milk')
+    inputPrice.setValue(15)
+    inputBrand.setValue('Lala')
+
+    button.trigger('submit')
+
+    expect(wrapper.emitted()).toHaveProperty('save-article')
+    expect(inputName.element.value).toBe('')
+    expect(inputPrice.element.value).toEqual(0)
+    expect(inputBrand.element.value).toEqual('')
+  })
 })
