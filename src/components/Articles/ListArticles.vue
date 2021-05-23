f0bd484715c2c4cea2fea38b62ac0822bcb076b4
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2021-05-23 17:30:56 -0500

    fix css styles

--- src/components/Articles/ListArticles.vue
+++ src/components/Articles/ListArticles.vue
@@ -2,13 +2,13 @@
   <div class="c-list-articles mt-4">
     <h3>List Articles</h3>
     <p v-if="articles.length === 0">Oops, apparently you don't have any articles, but don't worry you can add some</p>
-    <div v-for="(article, index) in articles" :key="index">
-      <Article
-        :name="article.name"
-        :price="article.price"
-        :brand="article.brand"
-      />
-    </div>
+      <div v-for="(article, index) in articles" :key="index" class="d-grid justify-content-center mb-2">
+        <Article
+          :name="article.name"
+          :price="article.price"
+          :brand="article.brand"
+        />
+      </div>
   </div>
 </template>
 
@@ -25,5 +25,6 @@ export default {
 
 <style scoped>
 .c-list-articles {
+  /*overflow-y: scroll;*/
 }
 </style>
