77753af4a87b99761a0b03827614db6013cdc608
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2021-05-23 17:15:00 -0500

    feat pass values via accessory

--- src/components/Articles/Article.vue
+++ src/components/Articles/Article.vue
@@ -3,7 +3,7 @@
     <div class="card-body">
       <h5 class="card-title">{{name}}</h5>
       <h6 class="card-subtitle mb-2 text-muted">{{ brand }}</h6>
-      <p class="card-text">{{price}}</p>
+      <p class="card-text">${{price}}</p>
       <button class="btn btn-warning">Update</button>
       <button class="btn btn-danger">Delete</button>
     </div>
--- src/components/Articles/ListArticles.vue
+++ src/components/Articles/ListArticles.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="c-list-articles mt-4">
+    <h3>List Articles</h3>
     <p v-if="articles.length === 0">Oops, apparently you don't have any articles, but don't worry you can add some</p>
-    <h3>{{ articles }}</h3>
+    <div v-for="(article, index) in articles" :key="index">
+      <Article
+        :name="article.name"
+        :price="article.price"
+        :brand="article.brand"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Article from '@/components/Articles/Article'
 export default {
   name: 'ListArticles',
+  components: { Article },
   props: {
     articles: Array
   }
